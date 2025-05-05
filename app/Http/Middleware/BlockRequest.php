d9768768f63cc28558995a07eca21c2c2d6bbd13
saloniamatteo <saloniamatteo@pm.me>
2025-05-05 11:19:05 +0200

    BlockRequest: Add missing import

--- app/Http/Middleware/BlockRequest.php
+++ app/Http/Middleware/BlockRequest.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class BlockRequest
 {
